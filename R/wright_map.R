2661e9d888ddc6e13557a7b7dc459196c4739f8d
Richard <134499177+rich-a-rd@users.noreply.github.com>
2026-03-16 14:36:42 +0000

    Readd data description and wright_map function

---
+++ R/wright_map.R
@@ -0,0 +1,17 @@
+
+#' Plot a wright map of a Rasch model
+#'
+#' @param mod A Rasch model created by `cg_rm()`
+#'
+#' @returns A ggplot object.
+#' @export
+#'
+#' @examples
+#' mod <- dichotomous |>
+#' cg_rm()
+#'
+#' mod |> wright_map()
+#'
+wright_map <- function(mod) {
+
+}
